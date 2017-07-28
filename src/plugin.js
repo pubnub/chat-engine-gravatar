4ec1402c7d5daeb9c3556ef7e7b51265379278f2
Ian Jennings <ian@meetjennings.com>
2017-07-28 18:10:45 -0500

    add jsdoc documentation

--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,19 @@
+/**
+* Update all {@link Users}'s state and add a gravatar property based on ```user.state().email```. Result assigned to ```user.state().gravatar```.
+* @module chat-engine-gravatar
+*/
+
 let gravatar = require('gravatar');
 
+/**
+* @function
+* @example
+*
+* chat = new ChatEngine.Chat(new Date().getTime());
+* user = new OCF.User('ian', {email: 'ian@pubnub.com'});
+* user.plugin(gravatar());
+* console.log(user.state().gravatar());
+*/
 module.exports = (config) => {
 
     class extension {
@@ -7,6 +21,10 @@ module.exports = (config) => {
 
             if(this.parent.state().email) {
 
+                /**
+                @member state()"."gravatar
+                @ceextends User
+                */
                 this.parent.update({
                     gravatar: gravatar.url(this.parent.state().email)
                 });
